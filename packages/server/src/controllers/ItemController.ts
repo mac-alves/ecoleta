cb620720ef39daee6315eba62f790e3256d8dba3
mac-alves <mauricio.bleck7@gmail.com>
2020-07-26 04:49:57 -0300

    config monorepo

--- packages/server/src/controllers/ItemController.ts
+++ packages/server/src/controllers/ItemController.ts
@@ -1,20 +1,20 @@
-import { Request, Response } from 'express';
-import knex from '../database/connection';
+import { Request, Response } from 'express'
+import knex from '../database/connection'
 
 class ItemController {
-    async index(request: Request, response: Response){
-        const itens = await knex('itens').select('*'); 
-        
-        const serializedItems = itens.map(item => { 
-            return {
-                id: item.id,
-                name: item.title,
-                image_url: `${process.env.APP_URL}/uploads/${item.image}`
-            }
-        })
-    
-        return response.json(serializedItems);
-    }
+  async index (request: Request, response: Response) {
+    const itens = await knex('itens').select('*')
+
+    const serializedItems = itens.map(item => {
+      return {
+        id: item.id,
+        name: item.title,
+        image_url: `${process.env.APP_URL}/uploads/${item.image}`
+      }
+    })
+
+    return response.json(serializedItems)
+  }
 }
 
-export default ItemController;
+export default ItemController
